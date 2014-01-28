31e72fbd3b96489de50872624b57ce88678d14ff
Hank Stoever <hstove@gmail.com>
2014-01-27 22:00:46 -0800

    Adds concurrency to learner

--- learn.go
+++ learn.go
@@ -9,38 +9,62 @@ import (
   "github.com/eaigner/shield"
   "strconv"
   "strings"
+  "runtime"
 )
 
 var logger *log.Logger
+var start = 1960
+var end = 2004
 
-func main() {
-  classifier := shield.New(
+func newClassifier () shield.Shield {
+  return shield.New(
     shield.NewEnglishTokenizer(),
     shield.NewRedisStore("127.0.0.1:6379", "", logger, ""),
   )
+}
+
+func worker(year int, done chan bool) {
+  classifier := newClassifier()
+  filename := fmt.Sprintf("names/yob%d.txt", year)
+  file, _ := os.Open(filename)
+  defer file.Close()
+
+  reader := csv.NewReader(file)
+  for {
+    record, err := reader.Read()
+    if err == io.EOF {
+        break
+    }
+
+    count, _ := strconv.ParseInt(record[2], 10, 8)
+    name := strings.ToLower(record[0])
+    count = count / 10 + 1
+    idx := 0
+    for idx <= int(count) {
+      err := classifier.Learn(record[1], name)
+      if err != nil {
+        fmt.Println("Error: ", err)
+      }
+      idx++
+    }
+  }
+  fmt.Println("finished parsing", filename)
+  done <- true
+}
+
+func main() {
+  nCPU := runtime.NumCPU()
+  runtime.GOMAXPROCS(nCPU)
+  fmt.Println("Number of CPUs: ", nCPU)
+
+  classifier := newClassifier()
   classifier.Reset()
 
-  for i := 1950; i <= 2012; i++ {
-    filename := fmt.Sprintf("names/yob%d.txt", i)
-    file, _ := os.Open(filename)
-    defer file.Close()
-
-    reader := csv.NewReader(file)
-    for {
-      record, err := reader.Read()
-      if err == io.EOF {
-          break
-      }
-
-      count, _ := strconv.ParseInt(record[2], 10, 8)
-      name := strings.ToLower(record[0])
-      count = count / 10 + 1
-      idx := 0
-      for idx <= int(count) {
-        classifier.Learn(record[1], name)
-        idx++
-      }
-    }
-    fmt.Println("finished parsing", filename)
+  done := make(chan bool, end - start)
+  for i := start; i <= end; i++ {
+    go worker(i, done)
+  }
+  for j := start; j <= end; j++ {
+    <-done
   }
 }
