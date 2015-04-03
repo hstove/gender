9ad7546b80e8408717cea96d158835dc9b214088
hank <hstove@gmail.com>
2015-04-03 15:26:07 -0700

    update web interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,23 @@
+function titleize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 $(function(){
   $('#gender').keyup(function(e){
     if (e.which == 13) {
-      var val = $(this).val()
+      var val = $(this).val();
       $.ajax({
         url: '/classify/' + val,
         dataType: 'json',
         success: function(data) {
-          $('#gender-result').html(data.gender)
-          $('#probability').html(data.probability + "%")
-          link = "http://gender.hankstoever.com/classify/"+val
-          $('#link a').attr('href',link).text(link)
+          $('.result-divider').removeClass('hidden');
+          var probability = new Number(data.probability).toFixed(2);
+          $('#gender-result').html(data.gender);
+          $('#probability').html(probability + "%");
+          link = "http://gender.hankstoever.com/classify/"+val;
+          $('#link a').attr('href',link).text(link);
+          $('#literate').text(probability + '% of Americans named '+ titleize(val) + ' are ' + data.gender + '.');
         }
-      })
+      });
     }
-  })
-})
+  });
+});
